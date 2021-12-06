9e4bce49428ea2d385588660b125e3568b4a847c
Kkrisztian08 <k.krisztian0830@gmail.com>
2021-12-06 09:51:30 +0100

    kész

--- src/main/java/com/example/calculator/HelloController.java
+++ src/main/java/com/example/calculator/HelloController.java
@@ -1,14 +1,59 @@
 package com.example.calculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label eredmeny;
+    @FXML
+    private Spinner<Double> elsoszam;
+    @FXML
+    private Spinner<Double> masodikszam;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void osszeadas() {
+        double elsoSzam=elsoszam.getValue();
+        double masodikSzam=masodikszam.getValue();
+        double muvelet=elsoSzam+masodikSzam;
+        eredmeny.setText(""+muvelet);
+
+
+    }
+
+    /*public  void initialize(){
+        elsoszam.getStyleClass().clear();
+        masodikszam.getStyleClass().clear();
+    }*/
+
+    public void szorzas(ActionEvent actionEvent) {
+        double elsoSzam=elsoszam.getValue();
+        double masodikSzam=masodikszam.getValue();
+        double muvelet=elsoSzam*masodikSzam;
+        eredmeny.setText(""+muvelet);
+    }
+
+    public void kivonas(ActionEvent actionEvent) {
+        double elsoSzam=elsoszam.getValue();
+        double masodikSzam=masodikszam.getValue();
+        double muvelet=elsoSzam-masodikSzam;
+        eredmeny.setText(""+muvelet);
+    }
+
+    public void osztas(ActionEvent actionEvent) {
+        double elsoSzam=elsoszam.getValue();
+        double masodikSzam=masodikszam.getValue();
+        double muvelet=elsoSzam/masodikSzam;
+        eredmeny.setText(""+muvelet);
+    }
+
+    public void maradekososztas(ActionEvent actionEvent) {
+        double elsoSzam=elsoszam.getValue();
+        double masodikSzam=masodikszam.getValue();
+        int muvelet=(int)elsoSzam % (int)masodikSzam;
+        eredmeny.setText(""+muvelet);
     }
 }
